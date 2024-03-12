fc85b640f4da47f5e07aa604b5051f0a15abdab7
QZHY-HTX <3191984534@qq.com>
2024-03-12 17:01:22 +0800

    update

--- 第二章/ch2-1.c
+++ 第二章/ch2-1.c
@@ -14,7 +14,7 @@ int main() {
     char ciphertext[MAX_SIZE];  
       
     // 使用 scanf 读取一行，直到遇到换行符或 EOF  
-    // 注意：scanf 不会自动读取换行符，需要额外处理  
+    // 注意 scanf 不会自动读取换行符，需要额外处理  
     // 这里使用 "%[^\n]" 格式说明符来读取直到换行符之前的所有字符  
     if (scanf("%[^\n]", ciphertext) != 1) {  
         // 如果没有读取到任何字符，则输出 NULL 并退出  
@@ -27,7 +27,7 @@ int main() {
     while ((c = getchar()) != '\n' && c != EOF);  
       
     // 解密并输出明文字符串  
-    
+
     for (int i = 0; ciphertext[i] != '\0'; i++) {  
         putchar(decode(ciphertext[i]));  
     }  
